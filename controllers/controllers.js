6b4a616107534bd123f57c5f937a524ad8097f35
Anthony <an538811@gmail.com>
2017-11-27 18:58:32 -0600

    abc

---
+++ controllers/controllers.js
@@ -0,0 +1,31 @@
+var express 		= require('express'),
+	router 			= express.Router(),
+	diagnoser 		= require('../models/diagnoser.js'),
+	request 		= require("request"),
+	passport 		= require("../config/passport"),
+	isAuthenticated = require("../config/middleware/isAuthenticated"),
+	current_user_id = 1;
+
+router.post('/register', function(req, res) {
+	userData = {
+		name: req.body.first_name + " " + req.body.first_name, 
+		phone: req.body.phone, 
+		email: req.body.email, 
+		gender: req.body.gender,
+		age: req.body.age, 
+		password: req.body.password
+	}
+	diagnoser.addUser(userData, (newlyCreatedId)=>{
+		current_user_id = newlyCreatedId;
+		res.render('login', {loggedIn:false})
+	});
+});
+
+router.get("/login", function(req, res) {
+	// If the user already has an account send them to the members page
+	if (req.user) {
+	  res.redirect("/member", {loggedIn:true});
+	}else{
+		res.render("login", {loggedIn:false});
+	}
+});
---
+++ models/user.js
@@ -0,0 +1,6 @@
+var orm = require('../config/orm.js');
+
+
+
+//package this as a bridge between the orm and controller
+module.exports = user;
