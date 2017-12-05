e5636a3aed9acbdff8def7416f9be4fac8adad86
Anthony <an538811@gmail.com>
2017-12-04 21:25:39 -0600

    end of day Ant

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,9 @@ var connection = require("./connection.js");
 var orm = {
 	//create a new user in the user table and send back the generated user_id
 	insertUser : function (userData, cbFunc){
-		let sql = "INSERT INTO users (name, phone, email, gender, age, password)" +
-					"VALUES (?, ?, ?, ?, ?, ?)"
-		connection.query(sql, [userData.name, userData.phone, userData.email, userData.gender,
+		let sql = "INSERT INTO users (name, phone, username ,email, gender, age, password)" +
+					"VALUES (?, ?, ?, ?, ?, ?, ?)"
+		connection.query(sql, [userData.name, userData.phone, userData.username, userData.email, userData.gender,
 			userData.age, userData.password], (error, results, fields)=>{
 				if (error){
 					console.log(error);
@@ -21,6 +21,18 @@ var orm = {
 		}
 		throw error;
 	},
+	addPosition : function(userData, cbFunc){
+		let sql = "INSERT INTO positions (role, fk_user_id, fk_team_id, time)" + "VALUES (?, ?, ?, ?)"
+		connection.query(sql, [positionData.role, positionData.fk_user_id, positionData.fk_team_id, Date.now()], (error, results, fields)=>{
+			if (error){
+				console.log(error);
+				throw error;
+			}
+			if(cbFunc && (typeof cbFunc === "function")){
+				cbFunc(results.insertId);				
+			}
+		});	
+	}
 	//get user information for the given username and send it to callback func. 
 	getUser : function(username, cbFunc){
 		let sql = "SELECT * FROM users WHERE email = ? LIMIT 1";
